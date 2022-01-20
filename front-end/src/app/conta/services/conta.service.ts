16926c0a7c069fea452416a00e432bc00906add5
Gabriel Porto <gr_porto@hotmail.com>
2022-01-19 23:04:01 -0300

    Adicionado comunicação com o back-end

--- front-end/src/app/conta/services/conta.service.ts
+++ front-end/src/app/conta/services/conta.service.ts
@@ -1,15 +1,27 @@
 import { HttpClient } from "@angular/common/http";
 import { Injectable } from "@angular/core";
+import { Observable } from "rxjs";
+import { catchError, map } from "rxjs/operators";
+import { BaseService } from "src/app/services/base.service";
 
 import { Usuario } from "../models/usuario";
 
 @Injectable()
-export class ContaService {
-    
-    constructor(private http: HttpClient) { }
+export class ContaService extends BaseService {
 
-    registrarUsuario(usuario: Usuario) {
-        console.log(usuario);
+    constructor(private http: HttpClient) { 
+        super();
+    }
+
+    registrarUsuario(usuario: Usuario): Observable<Usuario> {
+        let response = this.http
+            .post(this.UrlServiceV1 + 'nova-conta', usuario, this.ObterHeaderJson())
+            .pipe(
+                map(this.extractData),
+                catchError(this.serviceError)
+            );
+
+        return response;
     }
 
     login(usuario: Usuario) {

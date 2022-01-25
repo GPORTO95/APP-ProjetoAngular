d7228728ab6e4a491b3620c0ece5f6383761962b
Gabriel Porto <gr_porto@hotmail.com>
2022-01-24 22:04:34 -0300

    Ajuste no guarda de rotas

--- front-end/src/app/conta/services/conta.guard.ts
+++ front-end/src/app/conta/services/conta.guard.ts
@@ -1,7 +1,5 @@
 import { Injectable } from "@angular/core";
-<<<<<<< HEAD
-import { ActivatedRouteSnapshot, CanActivate, CanDeactivate, Router, RouterStateSnapshot, UrlTree } from "@angular/router";
-import { Observable } from "rxjs";
+import { CanActivate, CanDeactivate, Router} from "@angular/router";
 import { LocalStorageUtils } from "src/app/utils/localstorage";
 import { CadastroComponent } from "../cadastro/cadastro.component";
 
@@ -22,17 +20,6 @@ export class ContaGuard implements CanDeactivate<CadastroComponent>, CanActivate
 
     canDeactivate(component: CadastroComponent) {
         if (component.mudancasNaoSalvas) {
-=======
-import { ActivatedRouteSnapshot, CanDeactivate, RouterStateSnapshot, UrlTree } from "@angular/router";
-import { Observable } from "rxjs";
-import { CadastroComponent } from "../cadastro/cadastro.component";
-
-@Injectable()
-export class ContaGuard implements CanDeactivate<CadastroComponent> {
-    
-    canDeactivate(component: CadastroComponent) {
-        if(component.mudancasNaoSalvas) {
->>>>>>> 467626ac5d3024e999871ab8c97acbd2b5771a8a
             return window.confirm('Tem certeza que deseja abandonar o preenchimento do formulario?')
         }
 
